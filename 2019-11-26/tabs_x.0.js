28d0f3127c01eb50c5a3db6f67c53f741f874202
innerText <2217344225@qq.com>
2019-11-26 23:05:21 +0800

    006

---
+++ 2019-11-26/tabs_x.0.js
@@ -0,0 +1,13 @@
+class Tab{
+    constructor(that){
+        this.box = that;//jquery对象
+        this.opts = {
+            btns:['按钮一','按钮二','按钮三'],
+            content:[
+                '1111',
+                '222',
+                '333'
+            ]
+        }
+    }
+}
